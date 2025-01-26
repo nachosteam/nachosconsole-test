fa2260c76218c8b3628e5544b84dfd2198f23a17
TolyaGosuslugi <nikita2010belov@gmail.com>
2025-01-26 21:37:18 +0500

    тести саздания урааа

--- src/main.cpp
+++ src/main.cpp
@@ -23,11 +23,10 @@
 */
 
 #include <iostream>
+#include <fstream>
 #include <filesystem>
 #ifdef __WIN32
 	#include <windows.h>
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
 #elif __linux__
 	
 #endif
@@ -37,10 +36,24 @@ extern "C" {
 }
 
 int main(int argc, char *argv[]) {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
 	if (!std::filesystem::exists("nc-bin")) {
 		std::filesystem::create_directory("nc-bin");
 	}
-	if (!std::filesystem::exists("nc-bin/cfg.toml"))
+	if (!std::filesystem::exists("nc-bin/cfg.toml")) {
+		std::ofstream cfg;
+		cfg.open("nc-bin/cfg.toml");
+		if (cfg.is_open()) {
+			cfg << "pizdaaaa";
+			cfg.close();
+		}
+		else {
+			std::cout << "Can't open the file!";
+			return 1;
+		}
+	}
 	
 	/* auto data = toml::parse("test.toml");
 	int title = toml::find<int>(data, "main", "owners");
